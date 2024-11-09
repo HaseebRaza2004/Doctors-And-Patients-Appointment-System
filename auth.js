009eb5d0a17689418b1517993e622bf893e7f012
HaseebRaza2004 <156467139+HaseebRaza2004@users.noreply.github.com>
2024-11-09 04:24:24 -0800

    update auth.js

--- auth.js
+++ auth.js
@@ -3,25 +3,13 @@ import { UserModal } from "@/lib/models/userModel";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-async function handleLogin(obj) {
-    await connectDB();
-    const user = await UserModal.findOne({ email: obj.email });
-    if (user) {
-        return user;
-    } else {
-        let newUser = await UserModal(obj);
-        newUser = await newUser.save();
-        return newUser;
-    };
-};
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
     callbacks: {
         async signIn({ account, profile }) {
             if (account.provider === "google") {
-                // console.log("account", account);
-                // console.log("profile", profile);
+                console.log("account", account);
+                console.log("profile", profile);
                 let obj = {
                     firstName: profile.given_name,
                     lastName: profile.family_name,
@@ -36,12 +24,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             let user = await handleLogin({ email: token.email });
             token.role = user.role;
             token._id = user._id;
+            console.log("token", token);
             return token;
         },
         session({ session, token }) {
             session.user._id = token._id;
             session.user.role = token.role;
+            console.log("session", session);
             return session;
         },
     },
 });
+
+async function handleLogin(obj) {
+    await connectDB();
+    const user = await UserModal.findOne({ email: obj.email });
+    if (user) {
+        return user;
+    } else {
+        let newUser = await UserModal(obj);
+        newUser = await newUser.save();
+        return newUser;
+    };
+};
